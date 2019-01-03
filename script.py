93d5c026965a4ce180abec1485d16376e67849b6
AntonTretyak <33967517+AntonTretyak@users.noreply.github.com>
2019-01-03 21:39:41 +0200

    Add files via upload

---
+++ script.py
@@ -0,0 +1,28 @@
+'''print(date.year) 
+print(date.month)
+print(date.day)'''
+
+import datetime
+
+y = int(input('Введите год рождения: '))
+m = int(input('Введите месяц рождения: '))
+d = int(input('Введите день рождения: '))
+
+now = datetime.datetime.now()
+then = datetime.datetime(y, m, d)
+
+delta = now - then
+
+months = int(delta.days * 0.0329)
+weeks = int((delta.days * 0.0329) * 4)
+days = delta.days
+hours = delta.days * 24
+seconds = hours * 60
+years = months // 12
+
+print('Ты прожил',years,'лет,')
+print('Или',months,'месяцев,')
+print('Или',weeks,'недель,')
+print('Или',days,'дней,')
+print('Или',hours,'часов,')
+print('Или',seconds,'секунд.')
